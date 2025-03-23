8cfad17ba3530f7e332a2e775f2df9ff77d3af50
umumararungu <umcynth@gmail.com>
2025-03-23 20:19:41 +0200

    saved

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const path = require ('path')
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Middleware
